e38ca2c7b5f56c1317704d1dc982b6d9964daaeb
jc-alcaldev <carlos_juan1812@hotmail.com>
2020-10-27 17:15:37 +0000

    First Part finished

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,28 +2,8 @@ import React, { useState } from "react";
 import { TodosList } from "./TodosList.jsx";
 import { Input } from "./Input.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export function Home() {
-	/* const Action = () => {
-		const [todos, setTodos] = useState([]);
-		return {
-			todos,
-			addTodo: todoText => {
-				setTodos([...todos, todoText]);
-			},
-			deleteTodo: todoIndex => {
-				const newTodos = todos.filter(
-					(_, index) => index !== todoIndex
-				);
-
-				setTodos(newTodos);
-			}
-		};
-	};
-	const { todos, addTodo, deleteTodo } = Action([]); */
 	return (
 		<div className="d-flex justify-content-center">
 			<div className="bodyProject">
